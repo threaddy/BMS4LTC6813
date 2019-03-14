ee827ab74cfea475d6ad9073ca763210957e63f9
Threaddy <f.garbu@gmail.com>
2019-03-14 16:19:17 +0100

    puiogiu

--- bms.ino
+++ bms.ino
@@ -1,1 +1,76 @@
+/*!
+  DC2259
+  ltc6813-1: Battery stack monitor
 
+  REVISION HISTORY
+  $Revision: 1200 $
+  $Date: 2017-01-26
+
+  @verbatim
+
+  NOTES
+  Setup:
+   Set the terminal baud rate to 115200 and select the newline terminator.
+   Ensure all jumpers on the demo board are installed in their default positions from the factory.
+   Refer to Demo Manual DC2259.
+
+  USER INPUT DATA FORMAT:
+  decimal : 1024
+  hex     : 0x400
+  octal   : 02000  (leading 0)
+  binary  : B10000000000
+  float   : 1024.0
+  @endverbatim
+
+
+  Copyright (c) 2017, Linear Technology Corp.(LTC)
+  All rights reserved.
+
+  Redistribution and use in source and binary forms, with or without
+  modification, are permitted provided that the following conditions are met:
+
+  1. Redistributions of source code must retain the above copyright notice, this
+   list of conditions and the following disclaimer.
+  2. Redistributions in binary form must reproduce the above copyright notice,
+   this list of conditions and the following disclaimer in the documentation
+   and/or other materials provided with the distribution.
+
+  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+  ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+  WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+  DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+  ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+  (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+  ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+  (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+  SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+  The views and conclusions contained in the software and documentation are those
+  of the authors and should not be interpreted as representing official policies,
+  either expressed or implied, of Linear Technology Corp.
+
+  The Linear Technology Linduino is not affiliated with the official Arduino team.
+  However, the Linduino is only possible because of the Arduino team's commitment
+  to the open-source community.  Please, visit http://www.arduino.cc and
+  http://store.arduino.cc , and consider a purchase that will help fund their
+  ongoing work.
+
+  Copyright 2017 Linear Technology Corp. (LTC)
+*/
+
+
+#include <Arduino.h>
+#include <stdint.h>
+#include "Linduino.h"
+#include "LT_SPI.h"
+#include "UserInterface.h"
+#include "ltc681x.h"
+#include "ltc6813.h"
+#include <SPI.h>
+
+#define ENABLED 1
+#define DISABLED 0
+
+#define DATALOG_ENABLED 1
+#define DATALOG_DISABLED 0
